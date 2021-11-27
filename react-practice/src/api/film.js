4c89a5617bb8dbefbf33ce904d0b778b6586cbe4
kimjungjin <jungks9351@gmail.com>
2021-11-27 16:59:53 +0900

    feat : API 데이터 갯수 제한 쿼리 추가

--- react-practice/src/api/film.js
+++ react-practice/src/api/film.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export const getFilmInfo = async () => {
   try {
-    const res = await axios.get('https://ghibliapi.herokuapp.com/films');
+    const res = await axios.get(
+      'https://ghibliapi.herokuapp.com/films?limit=9',
+    );
     const filmData = res.data;
     return filmData;
   } catch (err) {
