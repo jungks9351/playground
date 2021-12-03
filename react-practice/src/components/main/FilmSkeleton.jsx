c9b0b7ea645c078a6f8819865eb94ac4bf684196
kimjungjin <jungks9351@gmail.com>
2021-12-03 16:59:55 +0900

    feat : useMemo 사용

--- react-practice/src/components/main/FilmSkeleton.jsx
+++ react-practice/src/components/main/FilmSkeleton.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ContentLoader from 'react-content-loader';
 
 const FilmSkeleton = () => {
+  const style = useMemo(
+    () => ({
+      boxShadow: '1px 5px 15px 1px rgba(0,0,0,0.2)',
+    }),
+    [],
+  );
+
   return (
     <>
       {new Array(9).fill(1).map((_, index) => {
@@ -13,7 +20,7 @@ const FilmSkeleton = () => {
             viewBox="0 0 400 400"
             backgroundColor="#f0f0f0"
             foregroundColor="#dedede"
-            style={{ boxShadow: '1px 5px 15px 1px rgba(0, 0, 0, 0.2)' }}
+            style={style}
           >
             <rect x="0" y="0" rx="4" ry="4" width="400" height="300" />
             <rect x="125" y="320" rx="0" ry="0" width="150" height="20" />
