38486aa0df95d2bb02f307f31503b727d5f97b51
kimjungjin <jungks9351@gmail.com>
2022-01-07 17:41:21 +0900

    feat : localStorage token 으로 헤더 메뉴 변경 구현

--- movie-app/src/components/common/CommonHeader.jsx
+++ movie-app/src/components/common/CommonHeader.jsx
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
 const CommonHeader = () => {
   const navigate = useNavigate();
 
+  const [token, setToken] = useState(false);
+
+  const getToken = () => {
+    const getToken = localStorage.getItem('token');
+    setToken(getToken);
+  };
+
+  const chnageNav = () => {
+    const clearToken = localStorage.clear();
+    setToken(clearToken);
+  };
+  useEffect(() => {
+    getToken();
+  }, [token]);
+
   return (
     <CommonHeaderWrapper>
       <h1>Popular Movie List</h1>
-      <nav className="right-nav">
-        <button
-          onClick={() => {
-            navigate('/login');
-          }}
-        >
-          로그인
-        </button>
-        <button
-          onClick={() => {
-            navigate('/regist');
-          }}
-        >
-          회원가입
-        </button>
-      </nav>
+      {!token ? (
+        <nav className="right-nav">
+          <button
+            onClick={() => {
+              navigate('/login');
+            }}
+          >
+            로그인
+          </button>
+          <button
+            onClick={() => {
+              navigate('/regist');
+            }}
+          >
+            회원가입
+          </button>
+        </nav>
+      ) : (
+        <nav className="right-nav">
+          <button onClick={chnageNav}>로그아웃</button>
+        </nav>
+      )}
     </CommonHeaderWrapper>
   );
 };
