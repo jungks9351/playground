60685122a420c584cdeeabf0a01a3a23dfbe4350
kimjungjin <jungks9351@gmail.com>
2022-01-15 16:18:10 +0900

    Chore react 라우팅 처리 초기 설정

---
+++ react-routing/src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <button>detail</button>
+      <button>login</button>
+      <button>register</button>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ react-routing/src/pages/RegisterPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return <h1>register</h1>;
+};
+
+export default RegisterPage;
