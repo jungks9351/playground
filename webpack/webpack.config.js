b9f07b0534a25fdfcce02fcf37dbc4a1760d87c8
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-07-05 15:48:25 +0900

    html plugin 설정

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  // mode 설정
   mode: 'development',
-  // 진입 포인트 설정
   entry: './src/index.js',
   output: {
-    // output으로 번들된 파일이름 지정
     filename: 'bundel.[hash].js',
   },
   module: {
@@ -26,4 +25,9 @@ module.exports = {
       },
     ],
   },
+  plugin: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
 };
