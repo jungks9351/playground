a080b7bbd09c68dde188e255a98c5ecff50d36e4
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-07-05 15:43:10 +0900

    loader 설정

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -7,4 +7,23 @@ module.exports = {
     // output으로 번들된 파일이름 지정
     filename: 'bundel.[hash].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules/',
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.html$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+          },
+        },
+      },
+    ],
+  },
 };
